3eae7f6d61062f710815e3f5ae175dcf454097ca
XoveR1 <vicheslav.p@tut.by>
2015-10-04 17:18:05 +0300

    new fixes

--- app/controls/ResultGrid.js
+++ app/controls/ResultGrid.js
@@ -61,6 +61,60 @@
                     element[0].appendChild(fragmentElement);
                 }
 
+
+                function createResultGrid3() {
+                    var tr = trElement.cloneNode();
+                    var td = tdElement.cloneNode();
+                    td.className = 'bold';
+                    td.textContent = 'Номер стимула';
+                    tr.appendChild(td);
+                    scope.result.IncentivesResults.forEach(function (intencive, index) {
+                        var td = tdElement.cloneNode();
+                        td.textContent = index + 1;
+                        tr.appendChild(td);
+                    });
+                    fragmentElement.appendChild(tr);
+
+                    var tr = trElement.cloneNode();
+                    var td = tdElement.cloneNode();
+                    td.className = 'bold';
+                    td.textContent = 'Количество воспринятых слов';
+                    tr.appendChild(td);
+
+                    scope.result.IncentivesResults.forEach(function (intencive) {
+                        var td = tdElement.cloneNode();
+                        td.textContent = intencive.countRememberedWords;
+                        tr.appendChild(td);
+                    });
+                    fragmentElement.appendChild(tr);
+
+                    var tr = trElement.cloneNode();
+                    var td = tdElement.cloneNode();
+                    td.className = 'bold';
+                    td.textContent = 'Из них, обладающих отличительным признаком,%';
+                    tr.appendChild(td);
+
+                    scope.result.IncentivesResults.forEach(function (intencive) {
+                        var td = tdElement.cloneNode();
+                        td.textContent = intencive.countRememberedWords;
+                        tr.appendChild(td);
+                    });
+                    fragmentElement.appendChild(tr);
+
+                    var tr = trElement.cloneNode();
+                    var td = tdElement.cloneNode();
+                    td.className = 'bold';
+                    td.textContent = 'Среднее количество воспринятых слов, обладающих отличительным признаком, %';
+                    tr.appendChild(td);
+                    var td = tdElement.cloneNode();
+                    td.textContent = scope.result.AverageRememberedRightWords;
+                    // td.setAttribute('colspan', 2);
+                    tr.appendChild(td);
+                    fragmentElement.appendChild(tr);
+
+                    element[0].appendChild(fragmentElement);
+                }
+
                 if (scope.experimentNumber === 1) {
                     createResultGrid1();
                 } else if (scope.experimentNumber === 2) {
@@ -68,7 +122,7 @@
                 } else if (scope.experimentNumber === 3) {
                     createResultGrid3();
                 } else if (scope.experimentNumber === 4) {
-                    createResultGrid4();
+                    createResultGrid3();
                 } else if (scope.experimentNumber === 5) {
                     createResultGrid5();
                 }
